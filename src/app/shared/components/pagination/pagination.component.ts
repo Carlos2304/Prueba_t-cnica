d354f079d62b9f1b51e2f64a52e4fa60af1e770c
Carlos Rodriguez <logdrix@gmail.com>
2022-09-12 10:47:08 -0500

    update some HttpClient and services

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -8,14 +8,13 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class PaginationComponent implements OnInit {
 
-  @Input() state:number = 0; //0 list, 1 add, 2 detail, 3 delete
   @Input() countList: number = 50; //Number of records fetch
-  @Input() count:number = 10; //Maximum number of records to display
-  @Input() currentPage:number = 1; //Current page
-  @Input() pageInitial:number = 1; //Shows the initial number of pagination
-  @Input() pageAverage:number = 2; //Shows the average number of pagination, change 
-  @Input() pageFinish:number = 3; //Shows the finish number of pagination, number maximum of pages
-  @Input() pageMax:number =3
+  count:number = 10; //Maximum number of records to display
+  currentPage:number = 1; //Current page
+  pageInitial:number = 1; //Shows the initial number of pagination
+  pageAverage:number = 2; //Shows the average number of pagination, change 
+  pageFinish:number = 3; //Shows the finish number of pagination, number maximum of pages
+  pageMax:number =3
 
   @Output() nextPage: EventEmitter<number> = new EventEmitter<number>();
   @Output() previousPage: EventEmitter<number> = new EventEmitter<number>();
@@ -28,7 +27,10 @@ export class PaginationComponent implements OnInit {
   }
 
   public onPreviousPage(): void {
+    if(this.currentPage>1){
     
+    }
+    this.previousPage.emit();
   }
 
 }
